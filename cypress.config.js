c204e759b68bb7fb716734028f612b518ec3d4bf
William Vianna <viannawp@gmail.com>
2024-09-12 15:40:32 -0300

    Criado documentacao, mobile viewport, evidencias com video e print no erro

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {},
   },
-})
+});
